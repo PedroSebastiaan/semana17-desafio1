2d9c0bd5a4dfb073124eac205d230a882d5aab28
PedroSebastiaan <pizarropedrov@gmail.com>
2021-10-04 22:24:00 -0300

    add: appcon functions to discount and use_coupon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,25 @@ class ApplicationController < ActionController::Base
 
     nil
   end
+
+  def discount (order, coupon)
+    if coupon.porcentual_discount != nil
+      order.total = @order.total*((100-coupon.porcentual_discount)/100)
+    elsif coupon.money_discount != nil
+      if order.total = @order.total - coupon.money_dicount > 0
+        order.total = @order.total - coupon.money_dicount
+      else
+        order.total = 0
+      end
+    end
+  end
+
+  def use_coupon (coupon)
+    if User.find(id: current_user.id).user_discounts.find(coupon_id: coupon.id) == nil
+      User.find(id: current_user.id).user_discounts.create(coupon_id: coupon.id)
+      redirect_to cart_path, notice: "Discount applied"
+    else
+      redirect_to cart_path, notice: "You already use this coupon"
+    end
+  end
 end
